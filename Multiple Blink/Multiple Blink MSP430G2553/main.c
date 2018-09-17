2eaf17fab5704f2ade4519c318d619c8136634c2
NickSetaro <33162219+NickSetaro@users.noreply.github.com>
2018-09-17 10:48:39 -0400

    Update main.c

--- Multiple Blink/Multiple Blink MSP430G2553/main.c
+++ Multiple Blink/Multiple Blink MSP430G2553/main.c
@@ -1,26 +1,35 @@
 #include <msp430.h> 
 
-
-
-/**
- * main.c
+/*
+ * @author Nick Setaro
+ * @version 9.14.2018
  */
+
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	P1SEL &= ~(BIT0 + BIT6);
-	P1SEL2 &= ~(BIT0 + BIT6);
-	P1DIR |= (BIT0 + BIT6);
+	P1SEL &= ~(BIT0 + BIT6);    //select LED
+	P1SEL2 &= ~(BIT0 + BIT6);   //select other LED
+	P1DIR |= (BIT0 + BIT6);     //set P1.0 and 1.6 to output
+
+	P1OUT |= BIT0;              //set LED on
+	P1OUT &= ~BIT6;             //set other LED off
 	
-	P1OUT |= BIT0;
-	P1OUT &= ~BIT6;
-
 	while (1) {
-	    P1OUT ^= BIT0; //toggle bit 0 to turn on/off LED
-	    __delay_cycles(200000); //delay for second LED
+	    unsigned int i, j;      //counters for delays
 
-	    P1OUT ^= BIT6; //toggle bit 6 to turn on/off LED
-	    __delay_cycles(50000); //delay for first LED
+	    i++;                    //increment delay counter
+	    j++;                    //increment delay counter
+
+	    if(i>3000)   {             //if i LED has waited long enough to blink
+	        i=0;                //reset i counter
+	        P1OUT ^= BIT0;       //toggle LED
+	    }
+
+	    if (j>30000) {          //if j LED has waited long enough to blink
+	        j=0;                //reset j counter
+	        P1OUT ^= BIT6;      //toggle LED
+	    }
 	}
 	return 0;
 }
