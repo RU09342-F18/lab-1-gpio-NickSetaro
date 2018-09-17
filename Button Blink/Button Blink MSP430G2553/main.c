34166e5aa002efd060265a30ba3046c1c564dd3b
NickSetaro <33162219+NickSetaro@users.noreply.github.com>
2018-09-17 10:44:09 -0400

    added comments

--- Button Blink/Button Blink MSP430G2553/main.c
+++ Button Blink/Button Blink MSP430G2553/main.c
@@ -1,11 +1,15 @@
-#include  <msp430g2553.h> // For the M430G2553 chip,
+#include  <msp430.h> // For the M430G2553 chip,
+
+/*
+ * @author Nick Setaro
+ * @version 9.14.2018
+ */
 
 void main(void)
 {
         int value = 0;
         WDTCTL = WDTPW + WDTHOLD;               // Stop watchdog timer
 
-        P1REN |= 0x08;                          //Turn on PullUp on????? How do you turn it off?
 
         P1DIR &= ~BIT3;                         // Push Port 1 P1.3 (push button) as input
         P1DIR |= BIT6;                          // Set P1.6 (LED) to output direction
