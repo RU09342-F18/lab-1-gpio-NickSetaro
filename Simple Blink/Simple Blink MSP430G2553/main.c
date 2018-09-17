b0d61917669a4af5344ac12a6fd679f924dbecd6
NickSetaro <33162219+NickSetaro@users.noreply.github.com>
2018-09-17 10:51:14 -0400

    Update main.c

--- Simple Blink/Simple Blink MSP430G2553/main.c
+++ Simple Blink/Simple Blink MSP430G2553/main.c
@@ -1,11 +1,15 @@
 #include <msp430.h>
 
+/*
+ * @author Nick Setaro
+ * @version 9.14.2018
+ */
 
 int main (void) {
     WDTCTL = WDTPW | WDTHOLD; //stop watchdog timer
     P1SEL &= ~BIT0;  // set path to LED
     P2SEL &= ~BIT0;
-    P1DIR |= BIT0;
+    P1DIR |= BIT0;  //set pin direction to output
     while (1) {
         P1OUT ^= BIT0; //toggle least significant bit
         __delay_cycles(200000); //delay
